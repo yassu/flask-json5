e0265d5a2caedad543d4e825421751546d49347d
yassu <yassu0320.dev@gmail.com>
2018-05-12 17:41:11 +0900

    update

--- json5_conv.py
+++ json5_conv.py
@@ -7,12 +7,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     json5_input = request.form.get('json5_input', '')
-    json_output = get_output_json_str(get_parsed_json5_obj(json5_input))
+
+    json5_obj = None
+    json_output = ""
+    error_message = ""
+    try:
+        json5_obj = get_parsed_json5_obj(json5_input)
+    except ValueError as e:
+        error_message = format(e)
+    if json5_obj is not None:
+        json_output = get_output_json_str(json5_obj)
+
     return render_template(
         'json5_conv.html',
         title='flask test',
         json5_input=json5_input,
-        json_output=json_output)
+        json_output=json_output,
+        error_message=error_message)
 
 def get_parsed_json5_obj(jstr):
     if jstr == "":
