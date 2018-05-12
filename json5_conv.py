9595e3d9c070ab45d60400b7c03038d4ed938654
yassu <yassu0320.dev@gmail.com>
2018-05-12 16:55:04 +0900

    save json5_input value

--- json5_conv.py
+++ json5_conv.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     json5_input = request.form.get('json5_input', '')
-    return render_template('json5_conv.html', title='flask test')
+    return render_template(
+        'json5_conv.html',
+        title='flask test',
+        json5_input=json5_input)
 
 ## おまじない
 if __name__ == "__main__":
