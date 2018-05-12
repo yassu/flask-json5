3a3d5bd7eeac40dd0e090cb4365d4fb4d0e1ef44
yassu <yassu0320.dev@gmail.com>
2018-05-12 16:24:39 +0900

    update

--- json5_conv.py
+++ json5_conv.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
+    print('form:')
+    print(request.form)
     name = "Hoge"
+    json5_text = request.form.get('single', '')
+    print('json5_text: {}'.format(json5_text))
     return render_template('json5_conv.html', title='flask test', name=name)
 
 ## おまじない
