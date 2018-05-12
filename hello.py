058ddbe23979d63036bef3c51754d5158794962e
yassu <yassu0320.dev@gmail.com>
2018-05-12 16:07:03 +0900

    Add templates

--- hello.py
+++ hello.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hello World"
-    return name
-
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+    name = "Hoge"
+    return render_template('hello.html', title='flask test', name=name)
 
 ## おまじない
 if __name__ == "__main__":
