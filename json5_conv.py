9ed4919d7bd2deb7a3a4ac134126043fe4db5e00
yassu <yassu0320.dev@gmail.com>
2018-05-12 17:04:57 +0900

    update some

--- json5_conv.py
+++ json5_conv.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     json5_input = request.form.get('json5_input', '')
+    json_output = "" if json5_input == "" else "outoutout"
     return render_template(
         'json5_conv.html',
         title='flask test',
-        json5_input=json5_input)
+        json5_input=json5_input,
+        json_output=json_output)
 
 ## おまじない
 if __name__ == "__main__":
